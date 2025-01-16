bbcd8e2df198c29c0a73c987b7b8410e84bf6abe
vibeslayer77 <garyreeves852@gmail.com>
2025-01-16 14:08:04 -0800

    Update constants.py

--- uniswap/constants.py
+++ uniswap/constants.py
@@ -1,5 +1,4 @@
 from typing import Set, cast
-
 from web3.types import RPCEndpoint  # noqa: F401
 
 # look at web3/middleware/cache.py for reference
@@ -12,7 +11,7 @@ SIMPLE_CACHE_RPC_WHITELIST = cast(
 )
 
 ETH_ADDRESS = "0x0000000000000000000000000000000000000000"
-WETH9_ADDRESS = "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2"
+WETH9_ADDRESS = "0x4200000000000000000000000000000000000006"
 
 # see: https://chainid.network/chains/
 _netid_to_name = {
@@ -32,6 +31,8 @@ _netid_to_name = {
     1666600000: "harmony_mainnet",
     1666700000: "harmony_testnet",
     11155111: "sepolia",
+    8453: "base",  # Added Base Mainnet
+    84532: "base_sepolia",  # Added Base Testnet (Sepolia)
 }
 
 _factory_contract_addresses_v1 = {
@@ -41,7 +42,6 @@ _factory_contract_addresses_v1 = {
     "kovan": "0xD3E51Ef092B2845f10401a0159B2B96e8B6c3D30",
     "görli": "0x6Ce570d02D73d4c384b46135E87f8C592A8c86dA",
 }
-
 
 # For v2 the address is the same on mainnet, Ropsten, Rinkeby, Görli, and Kovan
 # https://uniswap.org/docs/v2/smart-contracts/factory
@@ -53,10 +53,11 @@ _factory_contract_addresses_v2 = {
     "xdai": "0xA818b4F111Ccac7AA31D0BCc0806d64F2E0737D7",
     "binance": "0xcA143Ce32Fe78f1f7019d7d551a6402fC5350c73",
     "binance_testnet": "0x6725F303b657a9451d8BA641348b6761A6CC7a17",
-    # SushiSwap on Harmony
-    "harmony_mainnet": "0xc35DADB65012eC5796536bD9864eD8773aBc74C4",
+    "harmony_mainnet": "0xc35DADB65012eC5796536bD9864eD8773aBc74C4",  # SushiSwap on Harmony
     "harmony_testnet": "0xc35DADB65012eC5796536bD9864eD8773aBc74C4",
     "sepolia": "0x7E0987E5b3a30e3f2828572Bb659A548460a3003",
+    "base": "0x33128a8fC17869897dcE68Ed026d694621f6FDfD",  # Added Base Mainnet factory address
+    "base_sepolia": "0x02a84c1b3BBD7401a5f7fa98a384EBC70bB5749E",  # Added Base Testnet (Sepolia) factory address
 }
 
 _router_contract_addresses_v2 = {
@@ -64,13 +65,14 @@ _router_contract_addresses_v2 = {
     "ropsten": "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
     "rinkeby": "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
     "görli": "0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D",
-    "sepolia": "0xC532a74256D3Db42D0Bf7a0400fEFDbad7694008",
     "xdai": "0x1C232F01118CB8B424793ae03F870aa7D0ac7f77",
     "binance": "0x10ED43C718714eb63d5aA57B78B54704E256024E",
     "binance_testnet": "0xD99D1c33F9fC3444f8101754aBC46c52416550D1",
-    # SushiSwap on Harmony
-    "harmony_mainnet": "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
+    "harmony_mainnet": "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",  # SushiSwap on Harmony
     "harmony_testnet": "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",
+    "sepolia": "0xC532a74256D3Db42D0Bf7a0400fEFDbad7694008",
+    "base": "0x2626664c2603336E57B271c5C0b26F421741e481",  # Added Base Mainnet router address
+    "base_sepolia": "0x8cFe327CEc66d1C090Dd72bd0FF11d690C33a2Eb",  # Added Base Testnet (Sepolia) router address
 }
 
 MAX_UINT_128 = (2**128) - 1
